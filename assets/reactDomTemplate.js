4efe82644884c8081e132b1468fdcaefa9d0cdaf
reeli <reeli.null@gmail.com>
2018-10-16 21:44:04 +0800

    fix

--- assets/reactDomTemplate.js
+++
@@ -1,9 +0,0 @@
-const reactDomTemplate = (code, config, state) => {
-    const props = {};
-    let result = `import React from 'react'\n\n`;
-    result += `const ${state.componentName} = ${props} => ${code}\n\n`;
-    result += `export default ${state.componentName}`;
-    return result
-};
-
-module.exports = reactDomTemplate;
